9251a88b2df3199afadd79adb69cc8165c50be4d
andm2205 <andm22056@gmail.com>
2026-06-16 23:14:23 +0700

    Fix gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sharedflowdemo"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.sharedflowdemo"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
