fea7afd8e971cb676a3f96f69764399b9677ff38
andm2205 <andm22056@gmail.com>
2026-06-16 22:16:04 +0700

    Declare SharedFlow in ViewModel

--- app/src/main/java/DemoViewModel.kt
+++ app/src/main/java/DemoViewModel.kt
@@ -1,4 +1,15 @@
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 class DemoViewModel : ViewModel() {
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    init {
+        sharedFlowInit()
+    }
+
+    fun sharedFlowInit() {
+    }
 }
